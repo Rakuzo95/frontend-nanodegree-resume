07042cde8dee1686e6ec9bb384a1a30860d990ca
Andreas Lautenschläger <Andreas Lautenschlger>
2016-09-26 21:08:33 +0200

    26.09.16

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Andreas Lautenschläger");
+
+var name = "Andreas Lautenschläger";
+var role = "Web Developer";
+// var awesomeThoughts = "I am " + name + " and I am awesome!";
+// console.log(awesomeThoughts);
+
+// var funThoughts = awesomeThoughts.replace("awesome","fun");
+// $("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
